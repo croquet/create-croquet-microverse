6a550a5d73782e455803d801beb3a39587e08c66
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-06-20 09:59:32 -0700

    ready to test

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 function main() {
-    console.log("Hello from main");
-
     let { execSync } = require("child_process");
     let fs = require("fs");
+    let path = require("path");
 
     function execAndLog(...args) {
         try {
@@ -16,46 +15,103 @@ function main() {
             throw err;
         }
     }
+
+    function copyFileSync(source, target) {
+        let targetFile = target;
+
+        // If target is a directory, a new file with the same name will be created
+        if (fs.existsSync(target)) {
+            if (fs.lstatSync(target).isDirectory()) {
+                targetFile = path.join(target, path.basename(source));
+            }
+        }
+
+        fs.writeFileSync(targetFile, fs.readFileSync(source));
+    }
+
+    function copyFolderRecursiveSync(source, target) {
+        let files = [];
+
+        // Check if folder needs to be created or integrated
+        let targetFolder = path.join(target, path.basename(source));
+        if (!fs.existsSync(targetFolder)) {
+            fs.mkdirSync(targetFolder);
+        }
+
+        // Copy
+        if (fs.lstatSync(source).isDirectory()) {
+            files = fs.readdirSync(source);
+            files.forEach(( file ) => {
+                let curSource = path.join(source, file);
+                if (fs.lstatSync(curSource).isDirectory()) {
+                    copyFolderRecursiveSync(curSource, targetFolder);
+                } else {
+                    copyFileSync(curSource, targetFolder);
+                }
+            });
+        }
+    }
+    
+    function copyFiles() {
+        copyFolderRecursiveSync("node_modules/@croquet/microverse/behaviors", ".");
+        copyFolderRecursiveSync("node_modules/@croquet/microverse/assets", ".");
+        copyFolderRecursiveSync("node_modules/@croquet/microverse/worlds", ".");
+        copyFolderRecursiveSync("node_modules/@croquet/microverse/meta", ".");
+        copyFileSync("node_modules/@croquet/microverse/index.html", "./index.html");
+    }
     
     return new Promise((resolve, reject) => {
-        fs.writeFile(
-            "./package.json", `
+        fs.writeFile("./package.json", `
 {
   "name": "my-crqouet-microverse-world",
   "version": "1.0.0",
   "description": "A World in Croquet Microverse",
   "scripts": {
-    "start": "npm run file-server & npm run watch-server",
-    "file-server": "npx file-server --builder",
+    "start": "npm-run-all --parallel file-server & npm run watch-server",
+    "file-server": "npx file-server --port 9684",
     "watch-server": "npx watch-server"
   },
-  "author": "Croquet Corporation",
-  "license": "ISC",
-
   "dependencies": {
-    "@croquet/microverse-builder": "file:/Users/ohshima/src/microverse/npm"
+    "@croquet/microverse": "^0.1.0"
   },
   "devDependencies": {
-    "@croquet/microverse-watch-server": "file:/Users/ohshima/src/microverse-watch-server",
-    "@croquet/microverse-file-server": "file:/Users/ohshima/src/microverse-file-server"
+    "npm-run-all": "^4.1.5",
+    "@croquet/microverse-watch-server": "^1.0.5",
+    "@croquet/microverse-file-server": "^1.0.3"
   }
-}`.trim(),
-            {
-                encoding: "utf8",
-                flag: "w",
-                mode: 0o644
-            },
-            (err) => {
-                if (err) {
-                    console.log(err);
-                    return reject();
-                }
-                resolve();
-            }
-        )
+}`.trim(), {encoding: "utf8",
+            flag: "w",
+            mode: 0o644
+           }, (err) => {
+               if (err) {
+                   console.log(err);
+                   return reject();
+               }
+               resolve();
+           });
     }).then(() => {
-        execAndLog("npm install");
-        execAndLog("cp -r node_modules/@croquet/microverse-builder/.gitignore node_modules/@croquet/microverse-builder/.eslintrc.js node_modules/@croquet/microverse-builder/* .");
+        return new Promise((resolve, reject) => {
+            fs.writeFile("./apiKey.js", `
+const apiKey = "paste your apiKey from croquet.io/keys";
+const appId = "type your own appId such as com.example.david.mymicroverse";
+export default {apiKey, appId};
+
+// you may export other Croquet session parameters to override default values.
+`.trim(), {encoding: "utf8",
+           flag: "w",
+           mode: 0o644
+          }, (err) => {
+              if (err) {
+                  console.log(err);
+                  return reject();
+              }
+              resolve();
+          });
+        });
+    }).then(() => {
+        return execAndLog("npm install");
+    }).then(() => {
+        copyFiles();
     });
 }
 
