2c17d29f2abb3eab00a8ba670a05c612d3eddea9
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-10-11 15:38:04 -0700

    use 0.1.16

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function main() {
     "watch-server": "watch-server"
   },
   "dependencies": {
-    "@croquet/microverse-library": "0.1.15"
+    "@croquet/microverse-library": "0.1.16"
   },
   "devDependencies": {
     "npm-run-all": "^4.1.5",
