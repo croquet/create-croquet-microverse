2096de3b5d1458260c6ed90c444c29d81dd16616
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2023-08-04 10:32:35 -0700

    bump version 0.6.1

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function main() {
     "watch-server": "watch-server"
   },
   "dependencies": {
-    "@croquet/microverse-library": "0.5.2"
+    "@croquet/microverse-library": "0.6.1"
   },
   "devDependencies": {
     "npm-run-all": "^4.1.5",
